726448c1b0344616fa0f80a9da19ed1b362b4707
Rishika0304 <rishikavemparala@gmail.com>
2025-07-04 21:19:41 +0530

    Add files via upload

---
+++ Design-2/script.js
@@ -0,0 +1,17 @@
+function openProfile() {
+  alert(`🧑 Patient Profile:
+Name: Ram Kumar
+Age: 55
+Gender: Male
+Condition: Diabetes Type 2
+Allergies: None
+Contact: ramkumar@gmail.com
+Phone: +91-9876543210
+
+💊 Current Medications:
+- Metformin 500mg (Twice Daily)
+- Glimepiride 1mg (Once Daily)
+
+📅 Next Doctor Visit: 10th July 2025
+`);
+}
